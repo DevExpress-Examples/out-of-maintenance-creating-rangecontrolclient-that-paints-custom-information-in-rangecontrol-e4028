e5c30ceba6e23a3ce6f6d3417b994205efed2bfe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CustomRangeControlClient {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            rangeControl1.Client = new CustomRangeClient(101, -100, 100);
+
+            rangeControl1.SelectedRange.Maximum = 30;
+            rangeControl1.SelectedRange.Minimum = 10;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Creating RangeControlClient that paints custom information in RangeControl
+
+
+<p>You can implement your own Range Control Client to paint custom information within the <a href="https://documentation.devexpress.com/#WindowsForms/CustomDocument11739">Range Control</a>. To create a custom Client, create an object implementing the <a href="https://documentation.devexpress.com/CoreLibraries/clsDevExpressXtraEditorsIRangeControlClienttopic.aspx">IRangeControlClient</a> interface.</p>
+<p>The Range Control Client created in this example paints a graph within the Range Control.</p>
+
+<br/>
+
+
